52958ea48afb0d7d27ade711a044aa25da2839e3
fikriwafiuddin <fikriwafiuddin913@gmail.com>
2025-07-05 09:50:47 +0700

    fix: change cors configuration

--- src/application/web.js
+++ src/application/web.js
@@ -12,6 +12,7 @@ web.use(
     origin: process.env.FRONTEND_URL || "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 )
 web.use(express.json())
