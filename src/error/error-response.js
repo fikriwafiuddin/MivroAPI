e7e89d5ad3df3f7270e7cc842ada1605550fd6b8
fikriwafiuddin <fikriwafiuddin913@gmail.com>
2025-09-27 22:41:55 +0700

    start new

--- src/error/error-response.js
+++
@@ -1,9 +0,0 @@
-class ResponseError extends Error {
-  constructor(message, status = 500, data = {}) {
-    super(message)
-    this.status = status
-    this.data = data
-  }
-}
-
-export default ResponseError
--- src/server.js
+++
@@ -1,7 +0,0 @@
-import web from "./application/web.js"
-
-const PORT = process.env.PORT || 3000
-
-web.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
