77f7a9154f45948572fd31b7982b97200a121ffa
fikriwafiuddin <fikriwafiuddin913@gmail.com>
2025-10-22 22:02:14 +0700

    fix: fix error populate category in get category summary

--- src/services/reportService.js
+++ src/services/reportService.js
@@ -37,7 +37,7 @@ const getSummary = async (request, user) => {
     year,
     user,
   }).populate({
-    path: "category",
+    path: "categories.category",
     select: "name icon color",
     options: { includeDeleted: true },
   })
