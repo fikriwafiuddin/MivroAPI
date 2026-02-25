669627d0ff4c1f1a0b24a96fd410fcc747c52c5e
fikriwafiuddin <fikriwafiuddin913@gmail.com>
2026-02-25 15:35:12 +0700

    debug cors origin

--- src/routers/router.js
+++ src/routers/router.js
@@ -1,7 +1,7 @@
 import express from "express"
 import categoryRouter from "./categoryRouter.js"
 import errorMiddleware from "../middlewares/errorMiddleware.js"
-import { ErrorResponse } from "../utils/response.js"
+import { ErrorResponse, SuccessResponse } from "../utils/response.js"
 import transactionRouter from "./transactionRouter.js"
 import dashboardRouter from "./dashboardRouter.js"
 import reportRouter from "./reportRouter.js"
@@ -22,6 +22,15 @@ router.use("/chats", chatRouter)
 router.use("/settings", settingRouter)
 router.use("/feedback", feedbackRouter)
 router.use("/recurrings", recurringRouter)
+
+router.get("/", (req, res) => {
+  res.json(
+    new SuccessResponse(
+      `Welcome to Mivro API, ${process.env.CORS_ORIGIN}`,
+      200,
+    ),
+  )
+})
 
 router.all(/.*/, (req, res) =>
   res.status(404).json(new ErrorResponse("Route not found", 404)),
