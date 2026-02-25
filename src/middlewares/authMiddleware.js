067cbfb7438d6c7766c64298372ae2d1e8ee4ae6
fikriwafiuddin <fikriwafiuddin913@gmail.com>
2026-02-25 16:10:41 +0700

    debug auth middleware

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,5 +1,8 @@
+import logger from "../utils/logger.js"
+
 const authMiddleware = (req, res, next) => {
   const { userId } = req.auth()
+  logger.info(userId)
   if (!userId) {
     return res.status(401).json({
       message: "Unauthorized access",
